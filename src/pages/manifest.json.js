735dad3137eb732ad35db35e2df69622f58fe05f
Donnie D'Amato <fauxserious@gmail.com>
2024-06-07 22:46:06 -0400

    Storage

--- src/pages/manifest.json.js
+++ src/pages/manifest.json.js
@@ -7,6 +7,10 @@ const icons = [
     sizes: 'any',
     type: 'image/svg+xml'
   }, {
+    src: '/android-chrome-192x192.png',
+    sizes: '192x192',
+    type: 'image/png'
+  }, {
     src: '/android-chrome-512x512.png',
     sizes: '512x512',
     type: 'image/png'
